393940e612fdb46f3eb21afb99eba875f9282534
Fiodor Gorobet <iamfiogo@gmail.com>
2023-03-02 13:28:44 +0100

    #1 fix saving new user to db

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -1,10 +1,25 @@
+// import { PrismaClient } from '@prisma/client'
+
+// declare global {
+//   var prisma: PrismaClient | undefined
+// }
+
+// const client = globalThis.prisma || new PrismaClient()
+// if (process.env.NODE_ENV !== 'production') globalThis.prisma = client
+
+// export default client
+
 import { PrismaClient } from '@prisma/client'
 
-declare global {
-  var prisma: PrismaClient | undefined
-}
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
-const client = globalThis.prisma || new PrismaClient()
-if (process.env.NODE_ENV !== 'production') globalThis.prisma = client
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log:
+      process.env.NODE_ENV === 'development'
+        ? ['query', 'error', 'warn']
+        : ['error'],
+  })
 
-export default client
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,12 +1,11 @@
-import clientPromise from '@/lib/mongodb'
-import prisma from '@/lib/prismadb'
-import { MongoDBAdapter } from '@next-auth/mongodb-adapter'
-import { PrismaAdapter } from '@next-auth/prisma-adapter'
-import { randomBytes, randomUUID } from 'crypto'
-import NextAuth, { AuthOptions, Session } from 'next-auth'
-import { JWT } from 'next-auth/jwt'
-import GithubProvider from 'next-auth/providers/github'
+import { prisma } from '@/lib/prismadb';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { randomBytes, randomUUID } from 'crypto';
+import NextAuth, { AuthOptions, Session } from 'next-auth';
+import { JWT } from 'next-auth/jwt';
+import GithubProvider from 'next-auth/providers/github';
 
+// import prisma from '@/lib/prismadb'
 export const authOptions: AuthOptions = {
   secret: process.env.JWT_SIGNING_PRIVATE_KEY,
   adapter: PrismaAdapter(prisma),
