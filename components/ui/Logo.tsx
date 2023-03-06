0274873256874eeb0abb802affc0e43b8aa8a567
Fiodor Gorobet <iamfiogo@gmail.com>
2023-03-06 16:00:51 +0100

    add link to Logo component

--- components/ui/Logo.tsx
+++ components/ui/Logo.tsx
@@ -1,9 +1,11 @@
+import Link from 'next/link'
+
 const Logo: React.FC = () => {
   return (
-    <a className="text-xl normal-case btn btn-ghost">
+    <Link href="/" className="text-xl normal-case btn btn-ghost">
       <span className="text-xxl text-primary">Q</span>a
       <span className="text-xl text-primary">R</span>ds
-    </a>
+    </Link>
   )
 }
 
