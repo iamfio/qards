04b4b356b8dabcdd83d74c4381b2e332eeae3b1c
Fiodor Gorobet <iamfiogo@gmail.com>
2023-03-14 18:46:26 +0100

    make qard btns private

---
+++ app/[username]/page.tsx
@@ -0,0 +1,43 @@
+import ProfileCard from '@/components/profile/ProfileCard'
+import Qard from '@/components/qard/Qard'
+import { prisma } from '@/lib/globalPrisma'
+import { redirect } from 'next/navigation'
+
+type UserPageProps = {
+  params: { username: string }
+}
+
+const getUserByUsername = async (username: string) => {
+  return await prisma.user.findUnique({
+    where: { username },
+    include: { qards: true },
+  })
+}
+
+const UserPage = async ({ params }: UserPageProps) => {
+  const user = await getUserByUsername(params.username)
+
+  if (!user) {
+    redirect('/')
+  }
+
+  return (
+    <div>
+      <div className="snap-mandatory snap-always snap-y">
+        <div className="snap-start">
+          <ProfileCard user={user} />
+          {user.qards.map((qard) => (
+            <Qard
+              accountLink={qard.accountLink ?? ''}
+              accountName={qard.accountName ?? ''}
+              id={qard.id}
+              key={qard.id}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default UserPage
