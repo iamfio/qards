baa22055f080d91c9654c0b4c310656e727d5df7
Fiodor Gorobet <iamfiogo@gmail.com>
2023-03-16 14:25:03 +0100

    add username to auth typedef

--- auth.d.ts
+++ auth.d.ts
@@ -4,6 +4,7 @@ declare module 'next-auth' {
   interface Session {
     user: {
       id?: string | null // NOTE: made "id" optional (?) and nullable (null) so that it matches types with ones already existing in DefaultSession
+      username?: string | null
     } & DefaultSession['user']
   }
 }
