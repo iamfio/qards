c411ac49f87f33a5786966e3eecc3969b61586f9
Fiodor Gorobet <iamfiogo@gmail.com>
2023-03-20 14:49:18 +0100

    change logo size

--- components/ui/Logo.tsx
+++ components/ui/Logo.tsx
@@ -2,9 +2,9 @@ import Link from 'next/link'
 
 const Logo: React.FC = () => {
   return (
-    <Link href="/" className="text-xl normal-case btn btn-ghost">
-      <span className="text-xxl text-primary">Q</span>a
-      <span className="text-xl text-primary">R</span>ds
+    <Link href="/" className="text-3xl normal-case btn btn-ghost">
+      <span className="text-primary">Q</span>a
+      <span className="text-primary">R</span>ds
     </Link>
   )
 }
