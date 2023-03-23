dd27e0671b136af100bbff2df1974d9308795aca
Fiodor Gorobet <iamfiogo@gmail.com>
2023-03-23 18:11:52 +0100

    change text for no qards

--- app/[username]/page.tsx
+++ app/[username]/page.tsx
@@ -62,7 +62,7 @@ const UserPage = async ({ params }: UserPageProps) => {
                     d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
                   ></path>
                 </svg>
-                <span>You have no cards yet.</span>
+                <span>there is nothing here yet 🤷</span>
               </div>
             </div>
           )}
