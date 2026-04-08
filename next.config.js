f64d191068ce25741b1fee6c7d47e8cfc4e12d1b
Fiodor Gorobet <115066736+iamfio@users.noreply.github.com>
2026-04-08 14:47:49 +0200

    1. upgrade nextjs v. 13 to 14 2. replace beautiful-dnd with hello-pangea/dnd 3. clean up configurations

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   images: {
-    domains: ['avatars.githubusercontent.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+      },
+    ],
   },
 }
 
