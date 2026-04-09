554d98dfc7f6b63241c484536a2e89b68dd764f8
Fiodor Gorobet <115066736+iamfio@users.noreply.github.com>
2026-04-09 15:49:06 +0200

    fix typing for auth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,11 @@
 import { prisma } from '@/lib/prismadb'
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
 import { randomBytes, randomUUID } from 'crypto'
-import NextAuth, { AuthOptions, Session } from 'next-auth'
+import NextAuth, { type AuthOptions } from 'next-auth'
 
-import { JWT } from 'next-auth/jwt'
 import GithubProvider from 'next-auth/providers/github'
 import GoogleProvider from 'next-auth/providers/google'
+import type { User } from '@prisma/client'
 
 export const authOptions: AuthOptions = {
   secret: process.env.JWT_SIGNING_PRIVATE_KEY,
@@ -32,24 +32,18 @@ export const authOptions: AuthOptions = {
     },
   },
   callbacks: {
-    async session({
-      session,
-      user,
-      token,
-    }: {
-      session: Session
-      user: any
-      token: JWT
-    }) {
-      session.user.id = user.id
-      session.user.username = user.username
+    async session({ session, user }) {
+      if (session.user && user) {
+        session.user.id = user.id
+        session.user.username = (user as User).username
+      }
       return session
     },
-    async jwt({ token, user, account, profile, isNewUser }) {
+    async jwt({ token, user, trigger }) {
       return {
         token,
         user,
-        isNewUser,
+        trigger,
       }
     },
   },
