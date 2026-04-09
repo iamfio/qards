75f5106589c707230b002004ae41d8e07434a7e1
Fiodor Gorobet <115066736+iamfio@users.noreply.github.com>
2026-04-09 15:48:48 +0200

    fix code style

--- lib/utils/index.ts
+++ lib/utils/index.ts
@@ -1,10 +1,10 @@
 //* String Utils
 
-export const getFQDN = (url: string) => {
+export function getFQDN(url: string) {
   return new URL(url).hostname.replace('www.', '').split('.')[0]
 }
 
-export const getURL = (path: string) => {
+export function getURL(path: string) {
   const BASE_URL =
     typeof window === 'undefined'
       ? process.env.NEXT_PUBLIC_SITE_URL!
@@ -13,9 +13,10 @@ export const getURL = (path: string) => {
   return new URL(path, BASE_URL).toString()
 }
 
-export const isURL = (url: string) => {
+export function isURL(url: string) {
   return /^(?:\w+:)?\/\/([^\s\.]+\.\S{2}|localhost[\:?\d]*)\S*$/.test(url)
 }
 
-export const capitalize = (txt: string) =>
-  txt.charAt(0).toUpperCase() + txt.slice(1)
+export function capitalize(txt: string) {
+  return txt.charAt(0).toUpperCase() + txt.slice(1)
+}
